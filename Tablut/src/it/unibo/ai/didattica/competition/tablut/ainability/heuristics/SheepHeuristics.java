c642b3e30f853462db467eb2aca081ac19262887
LucaBerardi6 <berardiluca99@gmail.com>
2022-04-30 19:30:37 +0200

    fix bug

--- Tablut/src/it/unibo/ai/didattica/competition/tablut/ainability/heuristics/SheepHeuristics.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/ainability/heuristics/SheepHeuristics.java
@@ -74,6 +74,104 @@ public class SheepHeuristics extends Heuristics {
 	}
 
 	
+	/**
+	 * this method calculate the number of black pawns that are in danger--> that can be eaten after one move of opponent
+	 * @return number of pawns in danger
+	 */
+	protected int numberOfPawnsInDanger() {
+		
+
+		int number = 0;
+		Pawn [][] board = this.state.getBoard();
+		
+		for (int i = 0; i < board[0].length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j].equals(State.Pawn.WHITE)) { // MATCH!
+					int row = i+1; 		//+1 is necessary because we use 1-9 notation
+					int column = j+1;	//+1 is necessary because we use 1-9 notation
+					//----------------------------------------------------------------------------------------//
+					//Now let's check to adjacent threat already existing
+					//----------------------------------------------------------------------------------------//
+					//check north
+					if(row-1-1>=0 &&  board[row-1-1][column-1].equalsPawn(State.Pawn.BLACK.toString())  ||
+							this.isPositionCitadel(row-1-1,column-1) || board[row-1-1][column-1].equalsPawn(State.Pawn.THRONE.toString()))  
+					{//we have a potential threat on south side because of bad cells at north
+						if (row+1<=9 && this.canBlackPawnGoThere(row+1,column   ))number++ ;	
+					}
+					//check south	
+					if(row-1+1<=8 && ( board[row-1+1][column-1].equalsPawn(State.Pawn.BLACK.toString()) ) ||
+							this.isPositionCitadel(row-1+1,column-1) || board[row-1+1][column-1].equalsPawn(State.Pawn.THRONE.toString())  )
+					{//we have a potential threat on north side because of bad cells at south
+						if (row-1>=1 && this.canBlackPawnGoThere(row-1,column  ))number++ ;	
+					}	
+					//check west
+					if(column-1-1>=0 && (board[row-1][column-1-1].equalsPawn(State.Pawn.BLACK.toString()) ) ||
+							this.isPositionCitadel(row-1,column-1-1) || board[row-1][column-1-1].equalsPawn(State.Pawn.THRONE.toString())  )
+					{//we have a potential threat on east side because of bad cells at west
+						if (column +1<=9 && this.canBlackPawnGoThere(row,column +1   ))number++ ;	
+					}	
+					//check east
+					if(column-1+1 <=8 && ( board[row-1][column-1+1].equalsPawn(State.Pawn.BLACK.toString()) ) ||
+							this.isPositionCitadel(row-1,column-1+1) || board[row-1][column-1+1].equalsPawn(State.Pawn.THRONE.toString())  )
+					{//we have a potential threat on west side because of bad cells at east
+						if (column -1 >= 1 && this.canBlackPawnGoThere(row,column -1  ))number++ ;	
+					}						
+				}
+			}
+		}		
+		return number;
+		
+	}
+	
+	
+
+	protected boolean canBlackPawnGoThere(int posRow,int posColumn) {
+		Pawn [][] board = this.state.getBoard();
+		//check north side
+		for(int i=posRow-1-1;i>=0;i--) {
+			if(! board[i][posColumn-1].equalsPawn(State.Pawn.EMPTY.toString())  ) {
+					
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.BLACK.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;//exit for cycle
+			}
+		}
+		//check south side
+		for(int i=posRow-1+1;i<9;i++) {
+			if(! board[i][posColumn-1].equalsPawn(State.Pawn.EMPTY.toString())  ) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.BLACK.toString()) ){//the first pawn founded is white! DANGER
+					return true;
+				}else break;//exit for cycle
+			}
+		}
+		//check west side
+		for(int i=posColumn-1-1;i>=0;i--) {
+			if(! board[posRow-1][i].equalsPawn(State.Pawn.EMPTY.toString())) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.BLACK.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;
+			}
+		}
+		//check east side
+		for(int i=posColumn-1+1;i<9;i++) {
+			if(! board[posRow-1][i].equalsPawn(State.Pawn.EMPTY.toString())) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.BLACK.toString()) ){//the first pawn founded is white! DANGER
+					return true;
+				}else break;
+			}
+		}
+		
+		
+		
+		return false;
+	}
+	
+	
+	
 	
 
 	
--- Tablut/src/it/unibo/ai/didattica/competition/tablut/ainability/heuristics/WolfHeuristics.java
+++ Tablut/src/it/unibo/ai/didattica/competition/tablut/ainability/heuristics/WolfHeuristics.java
@@ -143,6 +143,108 @@ public class WolfHeuristics extends Heuristics {
 		return number;
 	}
 	
+	
+	/**
+	 * this method calculate the number of black pawns that are in danger--> that can be eaten after one move of opponent
+	 * @return number of pawns in danger
+	 */
+	protected int numberOfPawnsInDanger() {
+		
+
+		int number = 0;
+		Pawn [][] board = this.state.getBoard();
+		
+		for (int i = 0; i < board[0].length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				if (board[i][j].equals(State.Pawn.BLACK)) { // MATCH!
+					int row = i+1; 		//+1 is necessary because we use 1-9 notation
+					int column = j+1;	//+1 is necessary because we use 1-9 notation
+					//----------------------------------------------------------------------------------------//
+					//Now let's check to adjacent threat already existing
+					//----------------------------------------------------------------------------------------//
+					//check north
+					if(row-1-1>=0 && ( board[row-1-1][column-1].equalsPawn(State.Pawn.WHITE.toString()) ||board[row-1-1][column-1].equalsPawn(State.Pawn.KING.toString()) ||
+							this.isPositionCitadel(row-1-1,column-1) || board[row-1-1][column-1].equalsPawn(State.Pawn.THRONE.toString()))  )
+					{//we have a potential threat on south side because of bad cells at north
+						if (row+1<=9 && this.canPawnGoThere(row+1,column   ))number++ ;	
+					}
+					//check south	
+					if(row-1+1<=8 && ( board[row-1+1][column-1].equalsPawn(State.Pawn.WHITE.toString()) ||board[row-1+1][column-1].equalsPawn(State.Pawn.KING.toString()) ||
+							this.isPositionCitadel(row-1+1,column-1) || board[row-1+1][column-1].equalsPawn(State.Pawn.THRONE.toString())  ))
+					{//we have a potential threat on north side because of bad cells at south
+						if (row-1>=1 && this.canPawnGoThere(row-1,column  ))number++ ;	
+					}	
+					//check west
+					if(column-1-1>=0 && (board[row-1][column-1-1].equalsPawn(State.Pawn.WHITE.toString()) ||board[row-1][column-1-1].equalsPawn(State.Pawn.KING.toString()) ||
+							this.isPositionCitadel(row-1,column-1-1) || board[row-1][column-1-1].equalsPawn(State.Pawn.THRONE.toString())  ))
+					{//we have a potential threat on east side because of bad cells at west
+						if (column +1<=9 && this.canPawnGoThere(row,column +1   ))number++ ;	
+					}	
+					//check east
+					if(column-1+1 <=8 && ( board[row-1][column-1+1].equalsPawn(State.Pawn.WHITE.toString()) ||board[row-1][column-1+1].equalsPawn(State.Pawn.KING.toString()) ||
+							this.isPositionCitadel(row-1,column-1+1) || board[row-1][column-1+1].equalsPawn(State.Pawn.THRONE.toString())  ))
+					{//we have a potential threat on west side because of bad cells at east
+						if (column -1 >= 1 && this.canPawnGoThere(row,column -1  ))number++ ;	
+					}						
+				}
+			}
+		}		
+		return number;
+		
+	}
+	
+	
+
+	protected boolean canPawnGoThere(int posRow,int posColumn) {
+		Pawn [][] board = this.state.getBoard();
+		//check north side
+		for(int i=posRow-1-1;i>=0;i--) {
+			if(! board[i][posColumn-1].equalsPawn(State.Pawn.EMPTY.toString())  ) {
+					
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.WHITE.toString()) || board[i][posColumn-1].equalsPawn(State.Pawn.KING.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;//exit for cycle
+			}
+		}
+		//check south side
+		for(int i=posRow-1+1;i<9;i++) {
+			if(! board[i][posColumn-1].equalsPawn(State.Pawn.EMPTY.toString())  ) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.WHITE.toString()) || board[i][posColumn-1].equalsPawn(State.Pawn.KING.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;//exit for cycle
+			}
+		}
+		//check west side
+		for(int i=posColumn-1-1;i>=0;i--) {
+			if(! board[posRow-1][i].equalsPawn(State.Pawn.EMPTY.toString())) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.WHITE.toString()) || board[i][posColumn-1].equalsPawn(State.Pawn.KING.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;
+			}
+		}
+		//check east side
+		for(int i=posColumn-1+1;i<9;i++) {
+			if(! board[posRow-1][i].equalsPawn(State.Pawn.EMPTY.toString())) {
+				//inside this if only if the cells is not empty and is not a citadel
+				if(board[i][posColumn-1].equalsPawn(State.Pawn.WHITE.toString()) || board[i][posColumn-1].equalsPawn(State.Pawn.KING.toString())){//the first pawn founded is white! DANGER
+					return true;
+				}else break;
+			}
+		}
+		
+		
+		
+		return false;
+	}
+	
+	
+	
+	
+	
+	
 	/*
 	 * this method set the position of the king in the board and the number of
 	 * white/back pawns
